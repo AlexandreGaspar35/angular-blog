a4986efe10ba0449efb685cecea996042dd19a11
Alexandre <alexandre.d.s.gaspar@gmail.com>
2024-10-17 16:27:26 -0300

    Desacoplando os components da parte visual

--- src/app/component/big-card/big-card.component.ts
+++ src/app/component/big-card/big-card.component.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Input, input } from '@angular/core';
 
 @Component({
   selector: 'app-big-card',
   standalone: true,
   imports: [],
   templateUrl: './big-card.component.html',
-  styleUrl: './big-card.component.css'
+  styleUrl: './big-card.component.css',
 })
 export class BigCardComponent {
-
+  @Input()
+  photoCover: string = '';
+  @Input()
+  cardTitle: string = '';
+  @Input()
+  cardDescription: string = '';
 }
